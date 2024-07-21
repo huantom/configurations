78fd6303602f6ab29836f417b9ad9575e23f127f
differenteos <lb.huang@exsat.org>
2024-07-21 16:54:06 +0800

    init import

---
+++ src/buildList.js
@@ -0,0 +1,33 @@
+const { version } = require("../package.json");
+const synchronizers = require("./synchronizers/list.json");
+const validators = require("./validators/list.json");
+
+const bridgeUtils = require("@uniswap/token-list-bridge-utils");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  const l1List = {
+    name: "Uniswap Labs Default",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
+    keywords: ["exsat", "default"],
+    tokens: [
+      ...synchronizers,
+      ...validators,
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+  return bridgeUtils.chainify(l1List);
+};
